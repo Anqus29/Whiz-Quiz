6f1a1b51bd0998d80845221ac312c219aae1ab70
Angus <ABriscoe@student.saintaug.nsw.edu.au>
2025-03-18 09:58:55 +1100

    completed question choices and started asking questions

--- main2.py
+++ main2.py
@@ -29,13 +29,22 @@ def retrieve_questions(file_path):
 
 def display_categories(catagories):
     print("Topics available:")
-    for i, category in enumerate(catagories.keys()):
+    for i, category in enumerate(catagories.keys(), 1):
         print(f"{i}. {category}")
+    chosen_topic = int(input("What topic would you like to choose?\nEnter the number of the topic: "))
+    while chosen_topic < 1 or chosen_topic > len(catagories):
+        print("Invalid choice. Please select a valid number.")
+        chosen_topic = int(input("What topic would you like to choose?\nEnter the number of the topic: "))
+    return list(catagories.keys())[chosen_topic-1]
 
+def run_quiz(questions_by_category, chosen_topic):
+    print(f"\nStarting quiz in category: {chosen_topic}\n")
+    questions = questions_by_category[chosen_topic]
+    print(questions)
 
 def main():
     intro()
     questions_by_category = retrieve_questions("questions.csv")
-    display_categories(questions_by_category)
-    
+    chosen_topic = display_categories(questions_by_category)
+    run_quiz(questions_by_category, chosen_topic)
 main()
